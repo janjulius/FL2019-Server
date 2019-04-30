8f550b44809b882a18109fdbed41c3ca8e991a34
Jan Julius <jdelang@hotmail.com>
2019-04-30 19:44:31 +0200

    Added patching server files

---
+++ FLServer/FL-Patch_Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FL_Patch_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
