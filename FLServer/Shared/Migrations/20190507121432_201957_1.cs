9431a07eabd8b5b8b902054e7516fdb49e7407b5
Jan Julius de Lang <jdelang@hotmail.com>
2019-05-24 13:11:43 +0200

    Revert "Fixed Lvlexp generator + linux build"
    This reverts commit c5ea67af81998f07e24e8413f966d52124ea3b30.

---
+++ FLServer/Shared/Migrations/20190507121432_201957_1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FL_Login_Server.Migrations
+{
+    public partial class _201957_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UniqueIdentifier",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UniqueIdentifier",
+                table: "User");
+        }
+    }
+}
