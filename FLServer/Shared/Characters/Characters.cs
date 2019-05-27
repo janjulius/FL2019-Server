2509d021129cfe8019008dceaa8f4e0846284435
Jan Julius <jdelang@hotmail.com>
2019-05-27 18:06:59 +0200

    Characters class + creating method

---
+++ FLServer/Shared/Characters/Characters.cs
@@ -0,0 +1,51 @@
+﻿using FLServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Characters
+{
+    class Characters
+    {
+
+        public void CreateCharacter(
+         string name,
+         string desc,
+         string undertitle,
+         int dmg,
+         int ms,
+         int weight,
+         int attackspeed,
+         int range,
+         int size,
+         int def ,
+         DateTime releasedate,
+         int price,
+         int premiumprice)
+        {
+            using (FLDBContext ctx = new FLDBContext())
+            {
+                Character newChar = new Character()
+                {
+                    Name = name,
+                    Description = desc,
+                    UnderTitle = undertitle,
+                    Damage = dmg,
+                    MovementSpeed = ms,
+                    Weight = weight,
+                    AttackSpeed = attackspeed,
+                    Range = range,
+                    Size = size,
+                    Defense = def,
+                    ReleaseDate = releasedate,
+                    Price = price,
+                    PremiumPrice = premiumprice
+                };
+                ctx.Character.Add(newChar);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
