f54b94fdc9abeae233c7df5e084b37c8a7b3ff76
Jan Julius <jdelang@hotmail.com>
2019-05-29 20:18:25 +0200

    Migrations

---
+++ FLServer/Shared/Migrations/20190529173835_2019529_0.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shared.Migrations
+{
+    public partial class _2019529_0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
