5fa7a4a52ab546286d6bafc6e2567113e226753c
Wesley Hofma <hofm1600@student.nhl.nl>
2019-06-04 12:52:29 +0200

    added message table in database

---
+++ FLServer/Shared/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using FLServer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace Shared.Models
+{
+    public partial class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+
+        public int SenderId { get; set; }
+        public User User1 { get; set; }
+        public int ReceiverId { get; set; }
+        public User User2 { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
