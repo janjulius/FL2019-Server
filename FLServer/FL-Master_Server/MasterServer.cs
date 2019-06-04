42496054e29ac28c4d7fbfd1d1001b08b896153c
Jan Julius <jdelang@hotmail.com>
2019-06-04 19:52:06 +0200

    User is now forgotten once disconnected so no peer/user mismatch can occur

--- FLServer/FL-Master_Server/MasterServer.cs
+++ FLServer/FL-Master_Server/MasterServer.cs
@@ -407,6 +407,7 @@ namespace FL_Master_Server
         private static void OnListenerOnPeerDisconnectedEvent(NetPeer peer, DisconnectInfo info)
         {
             Console.WriteLine($"peer disconnected: {peer.EndPoint}");
+            MasterServer.Instance.NetworkUsers.Remove(MasterServer.Instance.util.GetNetworkUserFromPeer(peer));
         }
 
         public void SendNetworkEvent<T>(NetworkUser target, DeliveryMethod dm, ushort msgid, T packet) where T : struct
