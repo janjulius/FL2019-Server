542759e14040a278cca6af4e7fcc04c5cdf917a2
kiprok <arthur.vandenbroek@outlook.com>
2019-06-05 13:55:25 +0200

    changed position and rotation from array to struct

--- FLServer/FL-Game_Server/Packets.cs
+++ FLServer/FL-Game_Server/Packets.cs
@@ -1,72 +1,100 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace FL_Game_Server
+namespace FL_Game_Server.Packets
 {
-    public static class Packets
+    [Serializable]
+    public struct PlayerInfo
     {
-        [Serializable]
-        public struct PlayerInfo
+        public int networkId;
+        public int playerId;
+        public bool isHost;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
+        public string playerName;
+
+        public int characterId;
+        public int playerPlace;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public float[] playerColor;
+
+        public PlayerInfo(int networkId, int playerId, bool isHost, string playerName, int characterId, int playerPlace, float[] playerColor)
         {
-            public int networkId;
-            public int playerId;
-            public bool isHost;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
-            public string playerName;
-            public int characterId;
-            public int playerPlace;
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3) ]
-            public float[] playerColor;
-
-            public PlayerInfo(int networkId, int playerId, bool isHost, string playerName, int characterId, int playerPlace, float[] playerColor)
-            {
-                this.networkId = networkId;
-                this.playerId = playerId;
-                this.isHost = isHost;
-                this.playerName = playerName;
-                this.characterId = characterId;
-                this.playerPlace = playerPlace;
-                this.playerColor = playerColor;
-            }
+            this.networkId = networkId;
+            this.playerId = playerId;
+            this.isHost = isHost;
+            this.playerName = playerName;
+            this.characterId = characterId;
+            this.playerPlace = playerPlace;
+            this.playerColor = playerColor;
         }
-        
-        [Serializable]
-        public struct ObjectData
+    }
+
+    [Serializable]
+    public struct ObjectData
+    {
+        public int playerId;
+        public int objectId;
+        public int objectType;
+
+        public ObjectPositionData positionData;
+
+        public ObjectData(int playerId, int objectId, int objectType, Position pos, Rotation rot)
         {
-            public int playerId;
-            public int objectId;
-            public int objectType;
+            this.playerId = playerId;
+            this.objectId = objectId;
+            this.objectType = objectType;
 
-            public ObjectPositionData positionData;
-
-            public ObjectData(int playerId, int objectId, int objectType, float[] position, float[] rotation)
-            {
-                this.playerId = playerId;
-                this.objectId = objectId;
-                this.objectType = objectType;
-
-                positionData = new ObjectPositionData(position, rotation);
-            }
+            positionData = new ObjectPositionData(pos, rot);
         }
-        
-        [Serializable]
-        public struct ObjectPositionData
+    }
+
+    [Serializable]
+    public struct ObjectPositionData
+    {
+        public uint lastPacketId;
+
+        public Position position;
+        public Rotation rotation;
+
+        public ObjectPositionData(Position position, Rotation rotation)
         {
-            public uint lastPacketId;
-            
-            [MarshalAs(UnmanagedType.ByValArray,SizeConst = 3)]
-            public float[] position;
-
-            [MarshalAs(UnmanagedType.ByValArray,SizeConst = 4)]
-            public float[] rotation;
-
-            public ObjectPositionData(float[] position, float[] rotation)
-            {
-                this.position = position;
-                this.rotation = rotation;
-                lastPacketId = 0;
-            }
+            this.position = position;
+            this.rotation = rotation;
+            lastPacketId = 0;
         }
+    }
 
+    [Serializable]
+    public struct Position
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Position(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    [Serializable]
+    public struct Rotation
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public Rotation(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
     }
 }
