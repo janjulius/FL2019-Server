889e33d11b2b1b6949052b20f91555dbfd2bedc9
Jan Julius de Lang <jdelang@hotmail.com>
2019-06-06 12:08:58 +0200

    Updated packets and migrations for notification

---
+++ FLServer/Shared/Migrations/20190605112107_201956_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shared.Migrations
+{
+    public partial class _201956_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RequestType",
+                table: "FriendRequest",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestType",
+                table: "FriendRequest");
+        }
+    }
+}
