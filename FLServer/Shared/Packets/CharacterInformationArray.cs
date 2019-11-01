c5769f0d6e6a22e105ce87526bba94a0943cc317
Jan Julius <jdelang@hotmail.com>
2019-11-01 14:05:37 +0100

    modify: change back to old marshalling logic

--- FLServer/Shared/Packets/CharacterInformationArray.cs
+++ FLServer/Shared/Packets/CharacterInformationArray.cs
@@ -6,8 +6,8 @@ namespace Shared.Packets
     public struct CharacterInformationArray
     {
         int CharCount;
-        //[MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.PacketConstants.CharacterCount)]
-        [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ArrayMarshaler<CharacterInformation>))]
+        //[MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ArrayMarshaler<CharacterInformation>))]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.PacketConstants.CharacterCount)]
         public CharacterInformation[] chars;
 
         public CharacterInformationArray(int charCount, CharacterInformation[] chars)
